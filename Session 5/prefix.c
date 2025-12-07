736bcdc4666e250d772a89827caa5235cf037ed2
Letitia <116436351+letittya@users.noreply.github.com>
2025-12-07 13:59:29 +0200

    statements

---
+++ Session 5/prefix.c
@@ -0,0 +1,7 @@
+/*
+Implement a function char *extractPrefix(char *s1, char *s2) which takes two strings and returns another string 
+consisting of the longest common prefix of the two initial strings.
+
+I.e.: char s1[]="palindrome"; char s2[]="palisade"; should yield "pal"
+
+*/
---
+++ Session 5/reverse_vowel.c
@@ -0,0 +1,8 @@
+/*
+Implement a function char *reverseVowel(char *s) which takes a string consisting of words, separated by at 
+least one space (excepting the first and last word) and returns another string where each of the words 
+starting with vowel are reversed, leaving the other words unchanged. Keep the same amount of spaces between words.
+
+I.e.: "Anthony has a lot of apples and oranges" should yield into "yngotnA has a lot fo selppa dna segnaro"
+*/
+
